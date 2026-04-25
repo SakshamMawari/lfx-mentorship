85ea37352e4711f6cc24333f4bdd77343f526fef
Saksham Mawari <sakshammawari@gmail.com>
2026-04-25 11:35:31 +0530

    feat:Modified the function

--- src/parser.js
+++ src/parser.js
@@ -21,6 +21,16 @@ function extractJsonExtensions(jsonData) {
 }
 
 function extractExtensionSet(jsonData) {
-  
+  const set = new Set();
+
+  for (const instr in jsonData) {
+    let exts = jsonData[instr].extension || [];
+    if (!Array.isArray(exts)) exts = [exts];
+
+    exts.forEach((e) => set.add(e.toLowerCase()));
+  }
+
+  return set;
+}
 
 module.exports = { extractJsonExtensions, extractExtensionSet };
